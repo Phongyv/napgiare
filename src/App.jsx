5f0a20592c86724188f797841e5ab1e8a8e2bd6d
Nguyen Tien Phong <321phong@gmail.com>
2024-08-05 05:50:56 +0700

    Hello

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Home from "./App/Home";
+
+
+function App() {
+  return (
+    <Home></Home>
+  );
+}
+
+export default App;
---
+++ src/App/Home.jsx
@@ -0,0 +1,180 @@
+import '../Css/Home.css'
+import { Link } from 'react-router-dom';
+
+
+
+function Home(){
+    return(
+        <div className="home">
+
+          <header className='header'>
+          <img className='header-left' src='https://www.citypng.com/public/uploads/preview/hd-dragon-ball-z-kakarot-logo-png-701751694706719t409cehnxw.png' alt=''></img>
+          <div className='header-center'>
+            <Link className='header-center_item'>🏠 TRANG CHỦ</Link>
+            <Link className='header-center_item'>🛒 SHOP ACC</Link>
+            <Link className='header-center_item'>🎁 MINI GAME</Link>
+            <Link className='header-center_item'>💵 NẠP</Link>
+            <Link className='header-center_item'>📬 HỖ TRỢ</Link>
+          </div>
+          <img className='header-right' src='https://cdn-icons-png.flaticon.com/512/149/149071.png' alt=''></img>
+          </header>
+
+          <div className='body'>
+            <img className='body-top' src={'https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/VQ-KIM-CUONG-FREE-FIRE.gif?alt=media&token=5a2a9cb3-48bc-4d5b-b610-28ea01db4d26'} alt=''></img>
+
+          <div className='body-mid'>
+
+            <div className='body-mid_item'>
+              <span className='body-mid_item-text'>NẠP GAME</span>
+              <div className='body-mid_item-list'>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/Ban%20Robux%20Chinh%20Hang.gif?alt=media&token=f2fc27ff-1cb3-4489-b34a-bf8df3fc2ec5' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>ROBUX Chính Hãng</span>
+                    <span className='body-mid_item-list_item-info_description'>Số Giao Dịch: 324</span>
+                  </div>
+                </Link>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/DICH-VU-BAN-RP-LIEN-MINH.gif?alt=media&token=d2846f67-2f65-447d-94e4-8d743c784e6b' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>Bán RP Liên Minh</span>
+                    <span className='body-mid_item-list_item-info_description'>Số Giao Dịch: 608</span>
+                  </div>
+                </Link>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/DICH-VU-NAP-KIM-CUONG-FREE-FIRE.gif?alt=media&token=39ff3d25-a13c-4c39-aaf9-1786c41852af' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>Nạp Kim Cương FreeFire</span>
+                    <span className='body-mid_item-list_item-info_description'>Số Giao Dịch: 921</span>
+                  </div>
+                </Link>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/DICH-VU-NAP-QUAN-HUY.gif?alt=media&token=a5df6732-79ea-497a-bf12-f3455f455ad6' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>Nạp Quân Huy</span>
+                    <span className='body-mid_item-list_item-info_description'>Số Giao Dịch: 1209</span>
+                  </div>
+                </Link>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/Robux%20GamePass%20(120h).gif?alt=media&token=e5c00e2a-cb76-450f-9b95-e28724bccfd4' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>5 Ngày Giá Rẻ</span>
+                    <span className='body-mid_item-list_item-info_description'>Số Giao Dịch: 804</span>
+                  </div>
+                </Link>
+              </div>
+            </div>
+
+            <div className='body-mid_item'>
+              <span className='body-mid_item-text'>SHOP ACC</span>
+              <div className='body-mid_item-list'>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/ACC-MAX-ROBLOX-LEOPAD.gif?alt=media&token=5be84f97-a7e6-4f4d-bf66-ea1570b7d435' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>Acc Max Level ROBLOX LEOPAD</span>
+                    <span className='body-mid_item-list_item-info_description'>Số Tài Khoản: 237</span>
+                  </div>
+                </Link>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/BAN-ACC-ROBLOX-GIA-RE.gif?alt=media&token=68051ce6-d319-4fb8-acad-be7424d3995d' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>Acc ROBLOX Giá Rẻ</span>
+                    <span className='body-mid_item-list_item-info_description'>Số Tài Khoản: 79</span>
+                  </div>
+                </Link>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/MUA-NICK-FF-SIEU-RE.gif?alt=media&token=eec88f0c-8280-44de-ae71-541e6723cc0e' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>Nick FreeFire Siêu Rẻ</span>
+                    <span className='body-mid_item-list_item-info_description'>Số Tài Khoản: 148</span>
+                  </div>
+                </Link>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/RANDOM-FF-VIP-2.gif?alt=media&token=e133d19a-21ff-4236-aca5-91a88507db50' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>Random FreeFire VIP 2</span>
+                    <span className='body-mid_item-list_item-info_description'>Số Tài Khoản: 215</span>
+                  </div>
+                </Link>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/RANDOM-LQ-VIP-2.gif?alt=media&token=ca770835-6b10-4e7d-b976-7d7720a61575' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>Random Liên Quân VIP 2</span>
+                    <span className='body-mid_item-list_item-info_description'>Số Tài Khoản: 196</span>
+                  </div>
+                </Link>
+                
+              </div>
+            </div>
+
+            <div className='body-mid_item'>
+              <span className='body-mid_item-text'>MINI GAME</span>
+              <div className='body-mid_item-list'>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/VQ-KIM-CUONG-FREE-FIRE.gif?alt=media&token=5a2a9cb3-48bc-4d5b-b610-28ea01db4d26' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>Vòng Quay Kim Cương FreeFire</span>
+                    <span className='body-mid_item-list_item-info_description'>Đã Quay: 596</span>
+                  </div>
+                </Link>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/7oyQbW8QUU_1654049420.gif?alt=media&token=0efc27b4-499d-4e3e-b183-c98b1eab864b' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>Vòng Quay Trang Phục</span>
+                    <span className='body-mid_item-list_item-info_description'>Đã Quay: 623</span>
+                  </div>
+                </Link>
+              </div>
+            </div>
+
+            <div className='body-mid_item'>
+              <span className='body-mid_item-text'>DỊCH VỤ</span>
+              <div className='body-mid_item-list'>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/LIEN-MINH.gif?alt=media&token=fb72fc6e-063e-4787-aebb-5881a42df117' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>Cày Thuê Liên Minh</span>
+                    <span className='body-mid_item-list_item-info_description'>Giao Dịch: 273</span>
+                  </div>
+                </Link>
+                <Link className='body-mid_item-list_item'>
+                  <img className='body-mid_item-list_item-img' src='https://firebasestorage.googleapis.com/v0/b/pbet69-e8e73.appspot.com/o/LIEN-QUAN-MOBILE.gif?alt=media&token=606383b4-9a8d-4fae-9219-dbc218f18587' alt=''></img>
+                  <div className='body-mid_item-list_item-info'>
+                    <span className='body-mid_item-list_item-info_name'>Cày Thuê Liên Quân</span>
+                    <span className='body-mid_item-list_item-info_description'>Giao Dịch: 743</span>
+                  </div>
+                </Link>
+              </div>
+            </div>
+
+          </div>
+
+          <div className='body-bottom'>
+          <img className='body-bottom_img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiVGGGWtc0sD9hPRAHvMvz4xyyhYTngteZpIstjpgYuT-O4Invkqskbc4uP7En7hjgq78&usqp=CAU' alt=''></img>
+          <img className='body-bottom_img' src='https://play-lh.googleusercontent.com/nIV146CRuDyVKmYaXWtFR0BK7iZFqq4UyQPfY_iZOqolvk-USWmG9YupzKWDsN59fm6K=w240-h480-rw' alt=''></img>
+          <img className='body-bottom_img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYWgI-Qu8PxBhwz0Tc9McWWIxlxrCJZGNQdw&s' alt=''></img>
+          <img className='body-bottom_img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTertLujXbkXQug_W_MCCUZZJgZ-CijJ_msLw&s' alt=''></img>
+          <img className='body-bottom_img' src='https://play-lh.googleusercontent.com/PF5NUzoVxeU0cIwLCc5QYMtLcBJQ0TtQtN0OGfdZtheQIuBIiomqIvSQeo12bg8XLTgI' alt=''></img>
+          <img className='body-bottom_img' src='https://yt3.googleusercontent.com/4yd18H4ALHVFfjPPAnOYviNXm2dmQcR2dZru10wXA3ZkPJRkEV6yCuIodHnhJGG2e6mCGio=s900-c-k-c0x00ffffff-no-rj' alt=''></img>
+          <img className='body-bottom_img' src='https://i.pinimg.com/736x/40/eb/08/40eb08d0268f7df4e2083039d0608a92.jpg' alt=''></img>
+          <img className='body-bottom_img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv8HWhkPht9PEPTDDBtdF4jkX549Z1LKp-Nw&s' alt=''></img>
+          <img className='body-bottom_img' src='https://pbs.twimg.com/profile_images/1766224005181456384/gtmVrYa__400x400.jpg' alt=''></img>
+          <img className='body-bottom_img' src='https://seeklogo.com/images/C/csgo-logo-CAA0A4D48A-seeklogo.com.png' alt=''></img>
+
+          </div>
+
+          </div>
+
+
+          <footer className='footer'>
+            <span className='footer-title'>Uy tín của chúng tôi | Điều khoản sử dụng | Chính sách bảo mật</span>
+            <span className='footer-title'>Nạp Kim Cương Free Fire | Nạp Quân Huy Liên Quân | Nạp RP LOL | Nạp UC PUBG Mobile | Mua Ngọc NRO | Mua Xu Ninja School | Mua Robux | Nạp FC FO4 | Mua Vàng NRO | Mua Thẻ Game Giá Rẻ | Nạp Căn Nguyên Tốc Chiến | Nạp Game Auto | Nạp Game 24h | Nạp Game 3s | Nạp Game Siêu Rẻ | Nạp Game Nhanh  | Nạp Game Việt | Nạp Game Uy Tín | Nạp Liên Quân | Nạp Free Fire | Đổi thẻ cào sang momo</span>
+            <span className='footer-text'>© Copyright 2024 - Privacy Policy - Terms of Service</span>
+            <span className='footer-text'>Operated by Byyou, All Rights Reserved</span>
+          </footer>
+
+        </div>
+    );
+};
+
+export default Home;
